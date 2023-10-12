776074a8ce7af57718305f56a691fb2308452337
AngeloBerlini <angelo.berlini@studenti.isissgobetti.it>
2023-10-12 13:21:59 +0200

    creato esercizio 3

---
+++ esercizio_003.py
@@ -0,0 +1,7 @@
+#3. Chiedere in input il raggio.
+#Visualizzare la circonferenza e l’area del cerchio
+
+x=int(input("inserisci il raggio"))
+
+circonferenza=print(2*x*3.14)
+area=print((x**2)*3.14)

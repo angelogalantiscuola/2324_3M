0aa880ab8bc66983ee72bc1d3ff12d105283ab00
AlbertoBruscolini <147493117+AlbertoBruscolini@users.noreply.github.com>
2023-11-07 11:01:29 +0100

    Create esercizio_007.py

---
+++ esercizio_007.py
@@ -0,0 +1,10 @@
+number_1 = int(input("inserire numero: "))
+number_2 = int(input("inserire numero: "))
+number_3 = int(input("inserire numero:"))
+
+if number_1>number_2 and number_1>number_3:
+    print("il numero maggiore è", number_1)
+elif number_2>number_3 and number_2> number_1:
+    print("il numero maggiore è", number_2)
+elif number_3>number_1 and number_3>number_2:
+    print("il numero maggiore è", number_3)

f5ca0d2dc2de1451b2fbc3ba5b03afeda10cfbac
Matteo Massa <124779943+MatteoMassa@users.noreply.github.com>
2023-11-21 10:20:16 +0100

    Add files via upload

---
+++ esercizio_017.py
@@ -0,0 +1,9 @@
+list=[]
+numero_valori=int(input('quanti valori vuoi inserire nella lista?  '))
+for valore in range(numero_valori):
+    list.append(valore)
+se_valore=int(input('verifica il valore: '))
+if se_valore in list:
+    print('presente')
+else:
+    print('non presente')

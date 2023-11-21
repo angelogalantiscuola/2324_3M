8415a12b6fd73dbdfeed7100b069f5971409483b
Aron Bologna <147493559+AronBologna@users.noreply.github.com>
2023-11-21 10:28:10 +0100

    Add files via upload

---
+++ Bologna_006.py
@@ -0,0 +1,12 @@
+n1 = input("Inserisci il primo numero: ")
+n2 = input("Inserisci il secondo numero: ")
+
+n1 = int(n1)
+n2 = int(n2)
+
+if n1 % 2 == 0 and n2 % 2 == 0:  
+    print("Entrambi i numeri sono pari")
+elif n1 % 2 != 0 and n2 % 2 != 0:  
+    print("Tutti i numeri sono dispari")
+else: 
+    print("Un numero è pari e l'altro è dispari")

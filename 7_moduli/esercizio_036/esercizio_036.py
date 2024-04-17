9c26fb02edd521afb40990ad180d2a99ff3f1972
Angelo Galanti <angelo.galanti@gmail.com>
2024-04-17 09:59:15 +0200

    ex 37 created

--- 7_moduli/esercizio_036/esercizio_036.py
+++ 7_moduli/esercizio_036/esercizio_036.py
@@ -7,8 +7,31 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     # show a link to the table
-    return "Hello, World! <a href='/table/'>Table</a>"
+    return """
+    <a href='/dati_grezzi/'>Vai ai dati grezzi</a>
+    <br>
+    <a href='/dati_formattati/'>Vai ai dati formattati</a>
+    <br>
+    <a href='/table/'>Vai alla tabella</a>
 
+    """
+
+
+@app.route("/dati_grezzi/")
+def dati_grezzi():
+    with open("votes.json") as f:
+        data = json.load(f)
+    return data
+
+@app.route("/dati_formattati/")
+def dati_formattati():
+    with open("votes.json") as f:
+        data = json.load(f)
+    result = ''
+    for student in data:
+        result += f"{student['name']} ha preso {student['vote']}"
+        result += "<br>"
+    return result
 
 
 @app.route("/table/")
